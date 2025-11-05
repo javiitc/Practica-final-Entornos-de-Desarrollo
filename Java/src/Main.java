ba5e7c72cff773cea8719dfdec42ab05aa345c75
Javi <itsjavii1610@gmail.com>
2025-11-05 09:07:53 +0100

    Opciones

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Main {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
+        System.out.println("Seleccione el programa a ejecutar:");
+        System.out.println("1. Calculadora básica");
+        System.out.println("2. Clasificador de numeros");
+        System.out.println("3. Validador de contraseñas");
+        System.out.println("4. Generador de numeros aleatorios");
+        System.out.println("5. Contador de Palabras y caracteres");
+
     }
 }
